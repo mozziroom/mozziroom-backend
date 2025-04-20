68b8b57d98dd208c3a7caccea7c429142eb367f9
leejisuu <dlwltn966@gmail.com>
2025-04-20 20:55:47 +0900

    ✅ test: 이벤트 수정 API E2E 테스트 코드 작성

--- src/test/java/com/hhplus/project/interfaces/event/EventControllerTest.java
+++ src/test/java/com/hhplus/project/interfaces/event/EventControllerTest.java
@@ -1,17 +1,25 @@
 package com.hhplus.project.interfaces.event;
 
+import com.hhplus.project.domain.event.EventEnums;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EventControllerTest {
+
+    @LocalServerPort
+    int port;
 
     @Test
     @DisplayName("이벤트 상세 조회 API 호출 시 정상적으로 응답이 오는지 확인한다.")
@@ -35,5 +43,37 @@ class EventControllerTest {
         assertNotNull(response);
         assertEquals(status, "200");
         assertEquals(expectedResponse, eventDetailResponse);
+    }
+
+    @Test
+    @DisplayName("이벤트 정보 수정 API 호출 시, 정상 응답(HTTP 200)이 반환되는지 확인한다.")
+    void updateEvent() {
+        // given
+        long eventId = 1L;
+        UpdateEvent.Request request = new UpdateEvent.Request(
+                "서각코 모집",
+                LocalDateTime.of(2025, 4, 10, 14, 0),
+                LocalDateTime.of(2025, 4, 10, 16, 0),
+                30,
+                "서울 강남구 스타벅스",
+                EventEnums.ApproveType.AUTO,
+                null
+        );
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .port(port)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .patch("/events/" + eventId)
+                .then()
+                .log().all()
+                .extract();
+
+        // then
+        String status = response.jsonPath().getString("status");
+        assertEquals("200", status);
     }
 }
