4d27e4646c23ed0aa16f8856cfcefcf0b89e4c53
jo94kr <wls94kr@gmail.com>
2025-04-21 23:27:44 +0900

    🐛 fix:	ApiExceptionHandler 수정

--- src/main/java/com/hhplus/project/interfaces/member/MemberController.java
+++ src/main/java/com/hhplus/project/interfaces/member/MemberController.java
@@ -11,23 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/members")
 public class MemberController {
 
-    @PostMapping("")
-    public ResponseEntity<ApiResponse<MemberApiDto.SignUpResponse>> signUp(@RequestBody MemberApiDto.SignUpRequest request) {
-        return new ResponseEntity<>(
-                ApiResponse.of(
-                        HttpStatus.CREATED,
-                        "SignUp Complete",
-                        new MemberApiDto.SignUpResponse(1L)), HttpStatus.CREATED);
-
-    }
-
-    @PatchMapping("")
-    public ResponseEntity<ApiResponse<MemberApiDto.ModifyResponse>> modify(@RequestBody MemberApiDto.ModifyRequest request) {
-        return new ResponseEntity<>(
-                ApiResponse.of(
-                        HttpStatus.OK,
-                        "Modify Success",
-                        MemberApiDto.ModifyResponse.nameModified("김연습")), HttpStatus.OK);
-
-    }
+//    @PostMapping("")
+//    public ResponseEntity<ApiResponse<MemberApiDto.SignUpResponse>> signUp(@RequestBody MemberApiDto.SignUpRequest request) {
+//        return new ResponseEntity<>(
+//                ApiResponse.of(
+//                        HttpStatus.CREATED,
+//                        "SignUp Complete",
+//                        new MemberApiDto.SignUpResponse(1L)), HttpStatus.CREATED);
+//
+//    }
+//
+//    @PatchMapping("")
+//    public ResponseEntity<ApiResponse<MemberApiDto.ModifyResponse>> modify(@RequestBody MemberApiDto.ModifyRequest request) {
+//        return new ResponseEntity<>(
+//                ApiResponse.of(
+//                        HttpStatus.OK,
+//                        "Modify Success",
+//                        MemberApiDto.ModifyResponse.nameModified("김연습")), HttpStatus.OK);
+//
+//    }
 }
