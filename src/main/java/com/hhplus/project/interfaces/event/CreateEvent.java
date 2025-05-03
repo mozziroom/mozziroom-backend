4f852bd56d384085c693fa9a11c4ed6688f6fe36
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-05-04 00:46:28 +0900

    ♻️ refactor: CreateEvent Mock API 수정 - place -> location_id 수정 - category_id  추가 - is_online 추가

--- src/main/java/com/hhplus/project/interfaces/event/CreateEvent.java
+++ src/main/java/com/hhplus/project/interfaces/event/CreateEvent.java
@@ -11,14 +11,22 @@ public record CreateEvent() {
     public record Request (
             @Schema(description = "이벤트명", example = "서각코 모집")
             String name,
+
+            @Schema(description = "케테고리 Id", example = "2")
+            Long categoryId,
+
+            @Schema(description = "장소 Id", example = "지역 id")
+            Long location_id,
             @Schema(description = "이벤트 시작 일시", example = "2025-04-10T14:00:00")
             LocalDateTime startAt,
             @Schema(description = "이벤트 종료 일시", example = "2025-04-10T16:00:00")
             LocalDateTime endAt,
             @Schema(description = "정원", example = "30")
             int capacity,
-            @Schema(description = "장소", example = "서울 강남구 스타벅스")
-            String place,
+
+            @Schema(description = "온라인 모임인지 확인 하기 위한 flag",example = "N")
+            String is_online,
+
             @Schema(description = "승인 타입 (AUTO: 자동, MANUAL: 수동)", example = "AUTO")
             EventEnums.ApproveType approveType,
             @Schema(description = "반복 설정 정보")
