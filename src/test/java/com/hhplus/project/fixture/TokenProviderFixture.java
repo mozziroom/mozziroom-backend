4d90d6bbe21199660faf208d69708d75c5aef289
leejisuu <dlwltn966@gmail.com>
2025-06-23 21:56:18 +0900

    ✅ test: TokenProvider.java 단위 테스트 코드 작성

---
+++ src/test/java/com/hhplus/project/fixture/TokenProviderFixture.java
@@ -0,0 +1,24 @@
+package com.hhplus.project.fixture;
+
+import com.hhplus.project.domain.auth.TokenService;
+import com.hhplus.project.support.security.jwt.TokenProvider;
+
+public class TokenProviderFixture {
+
+    private static final String ACCESS_SECRET = "cdcq0Qt7yrsHkvI1uN2wA9nE1kBm4c5s0WCbaiHS3nY=";
+    private static final String REFRESH_SECRET = "vTmdmU+aQ6U14Psw0uAsgBi/TC8+6uk3Wc/kPnfTjSI=";
+
+    public static TokenProvider create(
+            long accessExpiration,
+            long refreshExpiration,
+            TokenService tokenService
+    ) {
+        return new TokenProvider(
+                ACCESS_SECRET,
+                accessExpiration,
+                REFRESH_SECRET,
+                refreshExpiration,
+                tokenService
+        );
+    }
+}
