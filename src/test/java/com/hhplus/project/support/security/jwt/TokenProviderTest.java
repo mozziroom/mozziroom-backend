ee634673e9b2d38ed65a862b5ae2308d137e2434
leejisuu <dlwltn966@gmail.com>
2025-06-27 11:48:26 +0900

    ✅ test: TokenProviderFixture.java 삭제 & TokenService 단위 테스트 작성

--- src/test/java/com/hhplus/project/support/security/jwt/TokenProviderTest.java
+++ src/test/java/com/hhplus/project/support/security/jwt/TokenProviderTest.java
@@ -1,7 +1,6 @@
 package com.hhplus.project.support.security.jwt;
 
 import com.hhplus.project.domain.auth.RefreshToken;
-import com.hhplus.project.fixture.TokenProviderFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,33 +8,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TokenProviderTest {
 
+    String ACCESS_SECRET = "cdcq0Qt7yrsHkvI1uN2wA9nE1kBm4c5s0WCbaiHS3nY=";
+    String REFRESH_SECRET = "vTmdmU+aQ6U14Psw0uAsgBi/TC8+6uk3Wc/kPnfTjSI=";
+    Long MEMBER_ID = 1L;
+    String ROLE = "ROLE_USER";
+
     @Test
     @DisplayName("Access Token 을 생성한 뒤, 토큰을 파싱해서 memberId와 권한 정보를 얻는다.")
     void createAccessToken() {
         // given
-        TokenProvider tokenProvider = TokenProviderFixture.create(10000, 10000);
-        Long memberId = 1L;
-        String role = "ROLE_USER";
+        TokenProvider tokenProvider = create(10000, 10000);
 
         // when
-        String token = tokenProvider.issueAccessToken(memberId, role);
+        String token = tokenProvider.issueAccessToken(MEMBER_ID, ROLE);
 
         // then
         assertThat(tokenProvider.validateAccessToken(token)).isTrue();
-        assertThat(tokenProvider.getMemberIdOfAccessToken(token)).isEqualTo(memberId);
-        assertThat(tokenProvider.getRoleOfAccessToken(token)).isEqualTo(role);
+        assertThat(tokenProvider.getMemberIdOfAccessToken(token)).isEqualTo(MEMBER_ID);
+        assertThat(tokenProvider.getRoleOfAccessToken(token)).isEqualTo(ROLE);
     }
 
     @Test
     @DisplayName("Refresh Token 을 생성한 뒤, 토큰을 파싱해서 memberId와 권한 정보를 얻는다.")
     void createRefreshToken() {
         // given
-        TokenProvider tokenProvider = TokenProviderFixture.create(10000, 10000);
-        Long memberId = 1L;
-        String role = "ROLE_USER";
+        TokenProvider tokenProvider = create(10000, 10000);
 
         // when
-        RefreshToken refreshToken = tokenProvider.issueRefreshToken(memberId, role);
+        RefreshToken refreshToken = tokenProvider.issueRefreshToken(MEMBER_ID, ROLE);
 
         // then
         String token = refreshToken.token();
@@ -48,12 +48,10 @@ class TokenProviderTest {
     @DisplayName("Access Token 이 만료되었으면 false 를 반환한다")
     void validateAccessTokenReturnFalse() throws InterruptedException {
         // given
-        TokenProvider tokenProvider = TokenProviderFixture.create(1000, 10000);
-        Long memberId = 1L;
-        String role = "ROLE_USER";
+        TokenProvider tokenProvider = create(1000, 10000);
 
         // when
-        String token = tokenProvider.issueAccessToken(memberId, role);
+        String token = tokenProvider.issueAccessToken(MEMBER_ID, ROLE);
         Thread.sleep(1100);
 
         // then
@@ -64,15 +62,17 @@ class TokenProviderTest {
     @DisplayName("Refresh Token 이 만료되었으면 false 를 반환한다")
     void validateRefreshTokenReturnFalse() throws InterruptedException {
         // given
-        TokenProvider tokenProvider = TokenProviderFixture.create(1000, 1000);
-        Long memberId = 1L;
-        String role = "ROLE_USER";
+        TokenProvider tokenProvider = create(1000, 1000);
 
         // when
-        RefreshToken refreshToken = tokenProvider.issueRefreshToken(memberId, role);
+        RefreshToken refreshToken = tokenProvider.issueRefreshToken(MEMBER_ID, ROLE);
         Thread.sleep(1100);
 
         // then
         assertThat(tokenProvider.validateRefreshToken(refreshToken.token())).isFalse();
+    }
+
+    private TokenProvider create(long accessExpiration, long refreshExpiration) {
+        return new TokenProvider(ACCESS_SECRET, accessExpiration, REFRESH_SECRET, refreshExpiration);
     }
 }
